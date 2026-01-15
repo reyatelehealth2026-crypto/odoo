d780d9c854898a519505938640c3e90e241c9a49
jameyannakon <newyannakon@gmail.com>
2026-01-16 02:04:09 +0700

    Add error display to check_contact_logs

--- install/check_contact_logs.php
+++ install/check_contact_logs.php
@@ -4,6 +4,10 @@
  * ตรวจสอบ log การตอบกลับคำสั่ง "ติดต่อ"
  */
 
+// Show all errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../classes/Database.php';
 
